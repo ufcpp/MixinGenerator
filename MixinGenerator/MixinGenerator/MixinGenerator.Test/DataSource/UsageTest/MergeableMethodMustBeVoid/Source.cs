caf17c18e40de77fdb792e15867e999643c2185e
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-06-06 18:22:59 +0900

    Add a testcase: a mergeable method must be void

---
+++ MixinGenerator/MixinGenerator/MixinGenerator.Test/DataSource/UsageTest/MergeableMethodMustBeVoid/Source.cs
@@ -0,0 +1,13 @@
+﻿using Mixins;
+
+namespace MixinGenerator
+{
+    public struct X
+    {
+        [Mergeable]
+        void M1() { } // OK
+
+        [Mergeable]
+        int M2() => 0 // NG
+    }
+}
