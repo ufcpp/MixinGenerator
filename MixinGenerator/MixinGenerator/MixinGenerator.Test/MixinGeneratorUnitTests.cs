ba12691dfb152138053cff63acf5cfca3c4069f2
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-12-22 11:21:48 +0900

    re-create sln with brand-new project template

---
+++ MixinGenerator/MixinGenerator/MixinGenerator.Test/MixinGeneratorUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using TestHelper;
+using Xunit;
+using MixinGenerator;
+
+namespace MixinGenerator.Test
+{
+    public class MixinGeneratorUnitTests : ConventionCodeFixVerifier
+    {
+        [Fact]
+        public void EmptySource() => VerifyCSharpByConvention();
+
+        [Fact]
+        public void LowercaseLetters() => VerifyCSharpByConvention();
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider() => new MixinGeneratorCodeFixProvider();
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new MixinGeneratorAnalyzer();
+    }
+}
